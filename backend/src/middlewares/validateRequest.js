08b21a33a504aa04221aac4be74a632981b37d42
marcosherna <marcosxdf6@gmail.com>
2025-10-12 11:58:45 -0600

    feat: request manager error

--- backend/src/middlewares/validateRequest.js
+++ backend/src/middlewares/validateRequest.js
@@ -1,17 +1,18 @@
 const { validationResult } = require("express-validator");
 
+const { BadRequest } = require("../errors");
+
 exports.validateRequest = (req, res, next) => {
   const errors = validationResult(req);
-
   if (!errors.isEmpty()) {
-    return res.status(400).json({
-      success: false,
-      errors: errors.array().map((err) => ({
-        field: err.param,
-        message: err.msg,
-      })),
-    });
+    console.log(errors);
+    const fields = errors.array().map((err) => ({
+      ...err,
+      property: err.path,
+      path: undefined,
+      value: undefined,
+    }));
+    throw new BadRequest("Validation request", fields);
   }
-
   next();
 };
