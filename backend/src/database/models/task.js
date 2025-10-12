81e8f134dc69423a484b2237e9d9e5bbc73d4b19
marcosherna <marcosxdf6@gmail.com>
2025-10-11 22:01:38 -0600

    build: add model task

---
+++ backend/src/database/models/task.js
@@ -0,0 +1,37 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Task extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Task.init(
+    {
+      title: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+      },
+      completed: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+      },
+    },
+    {
+      sequelize,
+      modelName: "Task",
+      tableName: "Tasks",
+      timestamps: true,
+    }
+  );
+  return Task;
+};
