01682c42911a82d20a578fea6af7cbdbcbe51eda
marcosherna <marcosxdf6@gmail.com>
2025-10-13 13:32:29 -0600

    feat: implement get, post and update method-

---
+++ frontend/api.js
@@ -0,0 +1,39 @@
+const API_URL = "http://localhost:3000/api/task";
+
+async function getTasks() {
+  const res = await fetch(API_URL);
+  if (!res.ok) throw new Error("Error al obtener tareas");
+  return await res.json();
+}
+
+async function getTask(taskId) {
+  const res = await fetch(`${API_URL}/${taskId}`);
+  if (!res.ok) throw new Error("Error al obtener la tarea");
+  return await res.json();
+}
+
+async function createTask({ title, description }) {
+  const res = await fetch(API_URL, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ title, description }),
+  });
+  if (!res.ok) throw new Error("Error al crear la tarea");
+  return await res.json();
+}
+
+async function updateTask(taskId, { title, description, completed }) {
+  const res = await fetch(`${API_URL}/${taskId}`, {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ title, description, completed }),
+  });
+  if (!res.ok) throw new Error("Error al actualizar la tarea");
+  return await res.json();
+}
+
+async function deleteTask(taskId) {
+  const res = await fetch(`${API_URL}/${taskId}`, { method: "DELETE" });
+  if (!res.ok) throw new Error("Error al eliminar la tarea");
+  return await res.json();
+}
